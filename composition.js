9797b3ac3244ae9efa6ee51489e4ffc51c262499
tharunkumarsk <tharunkumarsk@gmail.com>
2019-10-17 14:53:43 +0200

    Code without functions(compostion)

--- composition.js
+++ composition.js
@@ -1,4 +1,10 @@
-var someFunction = (x) =>{
-    return x ;
+var titleCase = (str) =>{
+    const parts = str.trim().split(' ');
+    const trimmed = parts.filter((next) => next.length > 0);
+    const capitalized = trimmed.map((next) => {
+    return next[0].toUpperCase() + next.substring(1);
+    });
+    return capitalized.join(' ') ;
+    
 };
-module.exports = someFunction;
+module.exports = titleCase;
--- spec/composition.spec.js
+++ spec/composition.spec.js
@@ -2,5 +2,5 @@ var Composition = require("../composition.js");
 
 it("should display default Player as 'X'", () => {
 
-    expect(Composition(3)).toEqual(3);
+    expect(Composition("tharun kumar")).toEqual("Tharun Kumar");
 });
